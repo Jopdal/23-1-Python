fc3e65d4e5cc02a0b34470952913ae2045c0bfc0
Jopdal <qqwwqq19@gmail.com>
2023-06-05 11:33:05 +0900

    230605 1132

---
+++ 23-1 Python/20230605 11장 class.py
@@ -0,0 +1,55 @@
+"""class Car:
+    color=""
+    speed=0
+
+    def upSpeed(self, value):
+        if value <= 150 :
+            self.speed += value
+        else :
+            self.speed = 150
+    
+    def downSpeed(self, value):
+        self.speed -= value
+
+myCar1 = Car()
+myCar1.color = "빨강"
+myCar1.speed = 0
+
+myCar2 = Car()
+myCar2.color = "파랑"
+myCar2.speed = 0
+
+myCar3 = Car()
+myCar3.color = "노랑"
+myCar3.speed = 0
+
+"""
+
+"""class BookReader :
+    name = str()
+    def read_book (self) :
+        print(self.name + 'is reading Book!!')
+    
+reader = BookReader()
+reader.name="Chris"
+reader.read_book()
+"""
+"""class Car :
+    speed=int()
+    color=str()
+
+    def __init__(self,val1,val2) :
+        self.speed=val1
+        self.color=val2
+    
+    def upspeed (self, value) :
+        self.speed+=value
+
+    def downspeed (self, value) :
+        self.speed-=value
+
+car1 = Car(10,'파랑')
+car2 = Car(30,'주황')
+car3 = Car(20,'빨강')
+"""
+
